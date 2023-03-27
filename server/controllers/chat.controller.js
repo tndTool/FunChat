a52bab344c7abc771d20eac0be593b57af715b91
I0ntdtn0I <19522362@gm.uit.edu.vn>
2023-03-27 10:05:48 +0700

    Fix bug

--- server/controllers/chat.controller.js
+++ server/controllers/chat.controller.js
@@ -1,7 +1,9 @@
 import { Configuration, OpenAIApi } from 'openai';
 
+const OPENAI_API_KEY = process.env.CHATGPT_API || '<your-key-here>';
+
 const openAIConfig = new Configuration({
-    apiKey: process.env.CHATGPT_API,
+    apiKey: OPENAI_API_KEY,
 });
 
 const openapi = new OpenAIApi(openAIConfig);
@@ -10,19 +12,24 @@ export const chatCompletion = async (req, res) => {
     try {
         const { prompt } = req.body;
 
+        if (!prompt) {
+            throw new Error('Prompt is required');
+        }
+
         const answer = await openapi.createCompletion({
             model: 'text-davinci-003',
-            prompt: prompt,
+            prompt,
             temperature: 0,
             max_tokens: 3000,
         });
 
-        const text = answer.data.choices[0].text;
-
-        res.status(200).json({ text });
-    } catch {
-        res.status(500).json({
-            message: err.message,
-        });
+        res.header('Access-Control-Allow-Origin', '*');
+        res.json({ text: answer.data.choices[0].text });
+    } catch (err) {
+        if (err.message === 'Prompt is required') {
+            res.status(400).json({ message: err.message });
+        } else {
+            res.status(500).json({ message: 'Internal server error' });
+        }
     }
 };
